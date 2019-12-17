1fe524c605a66f309f33b6fcfc2334c3c823533d
Kirill <kirillkulakow31@gmail.com>
2019-12-17 20:09:21 +0200

    add module 2

--- module-2/task-6.js
+++ module-2/task-6.js
@@ -7,12 +7,14 @@ let input;
 const numbers = [];
 let total = 0;
 
-    while (input !== null) {
-    input = prompt('Введите число');
-    if (Number.isNaN(Number(input))) {
-        alert ('Введите число!!!');
-    } else {
-        total = total + Number(input);
-        numbers.push(Number(input));}
-    }
-    alert (`Общая сумма чисел равна ${total}`)
+while (input !== null) {
+input = prompt('Введите число');
+if (Number.isNaN(Number(input)) || input === '') {
+    alert ('Введите число!!!');
+} else {
+    numbers.push(Number(input));}
+}
+for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i]
+}
+alert (`Общая сумма чисел равна ${total}`)
